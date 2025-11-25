0905e23b8a610cd3034338701bff03d85f8c1a09
tanbir12380 <mdkamrulhasantanbir0001@gmail.com>
2025-11-25 15:48:01 +0600

    get method for fetching bill data by id is added

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ async function run() {
       res.send(values);
     });
 
+    app.get("/bills/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection1.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
