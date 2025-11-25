eba360a679e022545f2acc146157bbc41d708d6f
tanbir12380 <mdkamrulhasantanbir0001@gmail.com>
2025-11-25 15:35:43 +0600

    mongodb is installed

--- index.js
+++ index.js
@@ -2,13 +2,44 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors");
+require("dotenv").config();
 app.use(cors());
 
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const userDB = client.db("Utility");
+    const userCollection1 = userDB.collection("AllBills");
+    const userCollection2 = userDB.collection("BillsPaid");
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
